4ced41dd26723537b543567f2fee3f7090498e67
Jemima <contatojemima@gmail.com>
2023-03-16 21:10:46 -0300

    Commit 15 - Criando a lista de dados

---
+++ tasks.js
@@ -0,0 +1,22 @@
+let tasksData = [
+  {
+    id: 1,
+    name: 'Ver se eu tô na esquina',
+    toDo: true
+  },
+  {
+    id: 2,
+    name: 'Dar banho nos gatos',
+    toDo: false
+  }
+];
+
+// new task
+
+// complete task
+
+// incomplete task
+
+// delete task
+
+// sync HTML with tasksData list
